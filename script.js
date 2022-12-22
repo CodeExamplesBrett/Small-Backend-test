69171199f2881106b05edd027f78b2b91806137a
Brett Scott <brett.scott@circet-networkservice.de>
2022-12-22 15:09:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+let users = [];
+        let item = "";
+        // let userDetails = {
+        // Name: 'fred',
+        // age: '31',
+        // sport: 'cricket'};
+
+
+        setURL('https://brett-scott.developerakademie.net/Backend_example/smallest_backend_ever');
+
+        
+
+        async function init() {
+            
+            await downloadFromServer();
+            users = JSON.parse(backend.getItem('users')) || [];
+
+            console.log('my', users);
+            displayEnties();
+
+            
+        }
+
+        function displayEnties(){
+            document.getElementById("names").innerHTML = ""
+            for (let i = 0; i < users.length; i++) {
+                user = users[i];
+                document.getElementById("names").innerHTML += `<span class="userName">${user} 
+                    <button onclick="deleteUser(${i})" >del</button>  
+                    <button onclick="editUser(${i})" >Edit</button>
+                    
+                     </span>`;
+            }
+
+        }
+
+        // function addDetails() {
+        //     users.push(userDetails);
+        //     backend.setItem('users', JSON.stringify(users));  
+        // }
+
+        function addUser() {
+            users.push(username.value);
+            setArray("users", users);
+            //backend.setItem('users', JSON.stringify(users));
+            displayEnties(); 
+        }
+
+        function deleteUser(i) {
+            users.splice(i, 1)
+            setArray("users", users);
+            displayEnties();  
+        }
+
+        function editUser(i) {
+            document.getElementById("inputBox").classList.remove('dnone')
+            item = i//updateUser(i);
+        }
+
+        function updateData(item) {
+            //let upate = document.getElementById('edit2').value;
+            console.log('it', item)
+
+            users[item] = edit2.value
+            useNew = users
+            console.log('new', useNew)
+            setArray("users", useNew);
+            //displayEnties(); 
+
+        }
+
+        
+        function updateUser(i) {
+            users[i] = edit.value
+            setArray("users", users);
+            displayEnties(); 
+        }
+
+
+
+        function setArray(key, array) {
+            backend.setItem(key, JSON.stringify(array));
+        }
